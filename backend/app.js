37417d89c4c9374726b38cf35f701d38c7635f2e
Zakirdin Lamiev <zakirdin.l@scopicsoftware.com>
2018-09-05 11:59:21 +0600

    adding express/node backend

---
+++ backend/app.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+
+const app = express();
+
+mongoose.connect("mongodb+srv://" + process.env.MONGO_ATLAS_USER + ":" + process.env.MONGO_ATLAS_PW + "@meanapp-bsxii.mongodb.net/" + process.env.MONGO_ATLAS_DB + "retryWrites=true", {useNewUrlParser: true}).then(() => {
+  console.log('Succesfully connected to MongoDB')
+}).catch((error) => {
+  console.log(error);
+  console.error('Connection to MongoDB is not successful')
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/', express.static(path.join(__dirname, 'dist')));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', "*");
+  res.setHeader('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept, authorization");
+  res.setHeader('Access-Control-Allow-Methods', "GET, POST, PATCH, PUT, DELETE, OPTIONS");
+  next();
+});
+
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"))
+});
+
+module.exports = app;
