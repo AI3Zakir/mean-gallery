555e2c17a2b18f2d1d014a30dc3b9b972bf6621f
Zakirdin Lamiev <zakirdin.l@scopicsoftware.com>
2018-09-05 18:51:44 +0600

    added proper login

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { UserService } from '../user.service';
+import { ParentErrorStateMatcher } from '../sign-up/sign-up.component';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
+  isLoading = false;
+  form: FormGroup;
+  private authStatusSubscription: Subscription;
 
-  constructor() { }
+  constructor(private userService: UserService) {
+  }
 
   ngOnInit() {
+    this.form = new FormGroup({
+      'email': new FormControl(null, {validators: [Validators.required, Validators.email]}),
+      'password': new FormControl(null, {validators: [Validators.required]}),
+    });
+
+    this.authStatusSubscription = this.userService.getAuthStatusListener().subscribe(
+      (authStatus) => {
+        this.isLoading = false;
+      }, (error) => {
+        this.isLoading = false;
+      }
+    );
+  }
+
+  onLogin() {
+    if (this.form.invalid) {
+      return;
+    }
+    this.isLoading = true;
+    this.userService.login(
+      this.form.value.email,
+      this.form.value.password,
+    );
+  }
+
+  ngOnDestroy() {
+    this.authStatusSubscription.unsubscribe();
   }
 
 }
